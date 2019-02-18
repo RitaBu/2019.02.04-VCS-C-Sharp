2829846d7d7d2a2a05b6932064eae129dde2e280
Rita <rita@trafi.com>
2019-02-18 13:48:07 +0200

    Add files via upload

---
+++ 8-pamoka/PagalbiniaiMetodai.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AstuntaPamoka
+{
+    public class PagalbiniaiMetodai
+    {
+        //1. Sukurti metodą, kuris nieko negrąžina ir atspausdina gautą tekstų masyvą, kiekvienas elementas turi būti atskirtas kableliais.
+        public static void AtspausdinkTekstus(string[] tekstai)
+        {
+            for (int i = 0; i < tekstai.Length; i++)
+            {
+                Console.Write($"{tekstai[i]}, ");
+            }
+        }
+
+        //2. Sukurti metodą, kuris nieko negrąžina ir atspausdiną gautą skaičių masyvą, kiekvienas elementas turi būti atskirtas kableliais.
+        public static void AtspausdinkSkaicius(int[] skaiciai)
+        {
+            for (int i = 0; i < skaiciai.Length; i++)
+            {
+                Console.Write($"{skaiciai[i]}, ");
+            }
+        }
+
+        //3. Sukurti metodą, kuris gauna tekstų masyvą ir grąžina tuos tekstus suklijuotus į vieną ir atskirtus kableliais.
+        public static string GrazinkVienaTeksta(string[] tekstai)
+        {
+            string vienasTekstas = "";
+
+            for (int i = 0; i < tekstai.Length; i++)
+            {
+                vienasTekstas += $"{tekstai[i]}, ";
+            }
+
+            return vienasTekstas;
+        }
+
+        //2. PagalbiniaiMetodai.cs faile sukurkite metodą, kuris moka paskaičiuoti duoto sąrašo sumą.
+        public static int PaskaiciuokSarasoSuma(List<int> skaiciai)
+        {
+            int suma = 0;
+
+            foreach (var skaicius in skaiciai)
+            {
+                suma += skaicius;
+            }
+
+            return suma;
+        }
+
+        //3. PagalbiniaiMetodai.cs faile sukurkite metodą, kuris moka paskaičiuoti duoto skaičių sąrašo vidurkį. (Šis metodas galėtų kviesti sumą skaičiuojantį metodą)
+        public static double PaskaiciuokSarasoVidurki(List<int> skaiciai)
+        {
+            double suma = PaskaiciuokSarasoSuma(skaiciai);
+            double vidurkis = suma / skaiciai.Count;
+            return vidurkis;
+        }
+
+        //4. Sukurkite metodą, kuris duotą skaičių sąrašą surūšiuoja mažėjimo tvarka.
+        public static void SurusiuokSarasaMazejimoTvarka(List<int> skaiciai)
+        {
+            skaiciai.Sort();
+            skaiciai.Reverse();
+        }
+    }
+}
--- 8-pamoka/Program.cs
+++ 8-pamoka/Program.cs
@@ -1,1 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace AstuntaPamoka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //metodai
+            SkaiciaiNuo100Iki1();
+            SkaiciaiNuo1IkiX(10);
+            int sandauga = Sandauga(9, 0);
+            Console.WriteLine(sandauga);
+
+            //metodai kitame faile
+            PagalbiniaiMetodai.AtspausdinkTekstus(new string[] {"vienas", "du", "trys"}); 
+            PagalbiniaiMetodai.AtspausdinkSkaicius(new int[] {1, 2, 3, 4, 5 }); 
+            var vienasTekstas = PagalbiniaiMetodai.GrazinkVienaTeksta(new string[] {"vienas", "du", "trys", "keturi"}); 
+            Console.WriteLine(vienasTekstas);
+
+            //sarasai ir metodai kitame faile
+            var skaiciai = new List<int> { 1, 99, 0, 50, 6, 78, 3, 4, 2 };
+            var suma = PagalbiniaiMetodai.PaskaiciuokSarasoSuma(skaiciai);
+            Console.WriteLine("saraso suma: " + suma);
+
+            var vidurkis = PagalbiniaiMetodai.PaskaiciuokSarasoVidurki(skaiciai);
+            Console.WriteLine("saraso vidurkis: " + vidurkis);
+
+            PagalbiniaiMetodai.SurusiuokSarasaMazejimoTvarka(skaiciai);
+
+            //LinQ
+            var balai = new List<int> { 1, 99, 0, 50, 6, 78, 3, 4, 4, 2, 99, 100, 100 };
+            var baluSuma = balai.Sum();
+            var min = balai.Min();
+            var max = balai.Max();
+            var baluVidurkis = balai.Average();
+            var pirmasElementas = balai.First();
+            var paskutinisElementas = balai.Last();
+            var nesikartojantysBalai = balai.Distinct();
+            var poaibis = balai.Skip(4).Take(5).ToList();
+
+            var lyginiaiSk = balai.Where(b => b % 2 == 0).ToList();
+            var didesniUz10MazesniUz20 = balai.Where(b => b > 10 && b < 20).ToList();
+            var skaiciuKvadratai = balai.OrderByDescending(b => b)
+                                        .Select(b => $"Skaicius = {b}, Kvadratas = {b * b}")
+                                        .ToList();
+
+            Console.Read();
+        }
+
+
+
+        //1. Parašyti metodą, kuris nieko negrąžina ir atspausdina skaičius nuo 100 iki 1.
+        public static void SkaiciaiNuo100Iki1()
+        {
+            for (int i = 100; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //2.  Parašyti metodą, kuris nieko negrąžina, bet gauna sveiką skaičių x ir atspausdina skaičius vienoje eilutėje su tarpais nuo 1 iki x.
+        public static void SkaiciaiNuo1IkiX(int x)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //3. Parašyti metodą, kuris gauna du sveikus skaičius ir grąžina jų sandaugą.Parašyti metodą, kuris gauna du tekstus ir grąžina juos sujungtus į vieną.
+        public static int Sandauga(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
