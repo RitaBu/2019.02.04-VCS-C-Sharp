d18fe18566b03bcff5ea871583980b511718204c
Rita <rita@trafi.com>
2019-02-18 21:39:33 +0200

    Add files via upload

---
+++ 10-pamoka/Viktorina/Viktorina.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesimtaPamoka
+{
+    public class Viktorina
+    {
+        public List<ViktorinosKlausimas> Klausimai { get; set; }
+        public int Taskai { get; set; }
+
+        public Viktorina(List<ViktorinosKlausimas> klausimai)
+        {
+            Klausimai = klausimai;
+            Taskai = 0;
+        }
+
+        public void PradetiZaidima()
+        {
+            for (int i = 0; i < Klausimai.Count; i++)
+            {
+                Klausimai[i].AtspausdinkKlausima();
+                bool arTeisingas = Klausimai[i].NuskaitykAtsakyma();
+                Console.WriteLine(arTeisingas);
+                if (arTeisingas)
+                {
+                    Taskai++;
+                }
+            }
+        }
+
+        public void ParodykRezultata()
+        {
+            Console.WriteLine("Surinkote tiek tasku: " + Taskai);
+        }
+    }
+}
---
+++ 10-pamoka/Viktorina/ViktorinosKlausimas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesimtaPamoka
+{
+    public class ViktorinosKlausimas
+    {
+        public string Klausimas { get; set; }
+        public string[] GalimiAtsakymai { get; set; }
+        public int TeisingasAtsakymas { get; set; }
+
+        public ViktorinosKlausimas(string klausimas, string[] galimiAtsakymai, int teisingasAts)
+        {
+            Klausimas = klausimas;
+            GalimiAtsakymai = galimiAtsakymai;
+            TeisingasAtsakymas = teisingasAts;
+        }
+
+        public void AtspausdinkKlausima()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(Klausimas);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < GalimiAtsakymai.Length; i++)
+            {
+                Console.Write($"{i+1}) {GalimiAtsakymai[i]}    ");
+            }
+            Console.WriteLine();
+        }
+
+        public bool NuskaitykAtsakyma()
+        {
+            Console.WriteLine("Iveskite atsakyma:");
+            int atsakymas = int.Parse(Console.ReadLine());
+            if (atsakymas - 1 == TeisingasAtsakymas)
+            {
+                return true;
+            }
+            else return false;
+            //return atsakymas - 1 == TeisingasAtsakymas;
+        }
+    }
+}
